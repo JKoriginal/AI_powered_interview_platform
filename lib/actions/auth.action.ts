bf4874d8764779f4af5dd5be42bba74777954566
JKoriginal <92023041+JKoriginal@users.noreply.github.com>
2025-04-11 20:32:46 +0530

    show generated interviews on the homepage

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import {auth, db} from "@/firebase/admin"
+import { DocumentData, Query } from "firebase-admin/firestore";
 import { cookies } from "next/headers";
 
 
@@ -152,3 +153,40 @@ export async function isAuthenticated() {
 
 
 }
+
+export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null>{
+
+    const interviews  =  await db
+        .collection('interviews')
+        .where('userId' , '==' , userId)
+        .orderBy('createdAt' , 'desc')
+        .get();
+
+        return interviews.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        })) as Interview[];
+
+
+} 
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null>{
+
+    const {userId ,limit = 20} = params ;
+
+    const interviews  =  await db
+        .collection('interviews')
+       
+        .orderBy('createdAt' , 'desc')
+        .where('userId' , '==' , true)
+        .where('userId' , '!=' , userId )
+        .limit(limit)
+        .get();
+
+        return interviews.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        })) as Interview[];
+
+
+} 
